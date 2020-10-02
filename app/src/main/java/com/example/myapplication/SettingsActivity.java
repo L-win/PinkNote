a8ba4664140abc85a00b3fd7a0096e711376ab78
L-win <l-win@inbox.ru>
2020-10-02 08:35:28 +0300

    Settings dark mode

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -1,15 +1,27 @@
 package com.example.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // SETTINGS
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean settingsDarkMode = preferences.getBoolean("check_box_dark_mode", true);
+        if(settingsDarkMode){
+            setTheme(R.style.PreferenceScreenDark);
+        }else {
+            setTheme(R.style.PreferenceScreen);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         setTitle("Settings");
